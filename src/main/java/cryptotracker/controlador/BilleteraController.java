fd099bee37887c5c7cafe190642de7db187bc2ad
Lucas <lucasubieta@gmail.com>
2025-04-25 13:05:05 +0200

    añade endpoint ticker

--- src/main/java/cryptotracker/controlador/BilleteraController.java
+++ src/main/java/cryptotracker/controlador/BilleteraController.java
@@ -1,13 +1,18 @@
 package cryptotracker.controlador;
 
-import cryptotracker.modelo.entity.*;
+import cryptotracker.modelo.entity.Billetera;
+import cryptotracker.modelo.entity.Criptomoneda;
+import cryptotracker.modelo.entity.Usuario;
 import cryptotracker.modelo.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/billetera")
@@ -148,6 +153,34 @@ public class BilleteraController {
     }
 
     
+    //LLAMADA API DE COINMARKETCAP 
     
-    
+    @RestController
+    @RequestMapping("/api/cripto")
+    @CrossOrigin(origins = "http://localhost:4200") // Angular local
+    public class CriptoController {
+
+        @GetMapping("/precios")
+        public ResponseEntity<?> obtenerPreciosCriptos(
+        	    @RequestParam(defaultValue = "1") int start,
+        	    @RequestParam(defaultValue = "50") int limit,
+        	    @RequestParam(defaultValue = "EUR") String convert) {
+            String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=1000&convert=USD";
+            String apiKey = "ea397464-a56e-442d-8ad2-74e9faeff22d";
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-CMC_PRO_API_KEY", apiKey);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            try {
+                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+                return ResponseEntity.ok(response.getBody());
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al consumir CoinMarketCap");
+            }
+        }
+    }
+
 }
